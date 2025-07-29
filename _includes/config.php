36f2047a703f48f093feb2161d0a0582af07ba42
Angga Novlianta <angganovliantast@gmail.com>
2025-07-30 01:00:35 +0700

    modified:   _includes/config.php

--- _includes/config.php
+++ _includes/config.php
@@ -1,4 +1,5 @@
 <?php
 // Definisikan URL dasar proyek Anda.
 define('BASE_URL', 'http://localhost/aac-freshmart/');
+define('GCS_BASE_URL', 'https://storage.googleapis.com/aacfreshmartnewbucket/products/');
 ?>
