7ab89a626f9b43db23673ac67bee59cd20268396
Angga Novlianta <angganovliantast@gmail.com>
2025-08-01 23:00:04 +0700

    Changes to be committed: modified:   _includes/db_connection.php Changes not staged for commit: modified:   _includes/db_connection.php

--- _includes/db_connection.php
+++ _includes/db_connection.php
@@ -3,10 +3,10 @@
 require_once 'config.php';
 
 // --- Kode koneksi database Anda yang sudah ada ---
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'db_aac_freshmart';
+$db_host = 'localhost'; // Tetap 'localhost'
+$db_user = 'u123456789_user_anda'; // Ganti dengan MySQL User dari Hostinger
+$db_pass = 'PasswordDatabaseAnda'; // Ganti dengan password yang Anda buat di Hostinger
+$db_name = 'u123456789_db_anda'; // Ganti dengan Nama Database dari Hostinger
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
